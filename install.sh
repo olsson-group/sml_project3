584bc160279d6d0b4b5864e8fde7bba4bc98cbe3
Selma <selma.moqvist@gmail.com>
2025-10-03 10:54:15 +0200

    remove openmm

--- install.sh
+++ install.sh
@@ -10,15 +10,12 @@ else
     pip install mdtraj
     pip install torch
     pip install torch_geometric
-    pip install torchdyn
+    pip install matplotlib
     
     pip cache purge
     pip install torch-scatter -f https://data.pyg.org/whl/torch-$(python3 -c "import torch; print(torch.__version__)")+cu$(python3 -c "import torch; print(torch.version.cuda.split('.')[0]+torch.version.cuda.split('.')[1])")/torch_scatter.html
     pip install torch-cluster -f https://data.pyg.org/whl/torch-$(python3 -c "import torch; print(torch.__version__)")+cu$(python3 -c "import torch; print(torch.version.cuda.split('.')[0]+torch.version.cuda.split('.')[1])")/torch_cluster.html
     
-    conda install -c conda-forge openmm --yes
-    conda install -c conda-forge openff --yes
-    conda install -c conda-forge openff-toolkit --yes
     conda install -c conda-forge mdtraj --yes
 fi
 
