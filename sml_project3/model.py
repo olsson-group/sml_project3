fe2103974f9531e42c2caa481224359d2ff4770e
Selma <selma.moqvist@gmail.com>
2025-10-03 10:57:59 +0200

    remove torchdyn, replace with euler solver

--- sml_project3/model.py
+++ sml_project3/model.py
@@ -1,8 +1,4 @@
 import torch
-import torch_geometric as geom
-from torchdyn.core import NeuralODE
-
-from sml_project3 import utils
 
 
 class CFM(torch.nn.Module):
@@ -13,14 +9,23 @@ class CFM(torch.nn.Module):
         self.basedistribution = basedistribution
 
         self.setup_optimizer()
+        self.setup_lr_scheduler()
 
     def training_step(self, loss):
         loss.backward()
         self.optimizer.step()
         self.optimizer.zero_grad()
 
+    def on_epoch_end(self, epoch_loss):
+        self.lr_scheduler.step(epoch_loss)
+
     def setup_optimizer(self, lr=0.001):
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+    def setup_lr_scheduler(self):
+        self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            self.optimizer, mode='min', factor=0.5, patience=5, min_lr=1e-7
+        )
 
     def forward(self, t, batch):
         batch = batch.clone()
@@ -42,42 +47,43 @@ class CFM(torch.nn.Module):
         return loss
 
     def sample_conditional_pt(self, t, x0, x1, batch_size):
-        epsilon = self.basedistribution.sample(batch_size) * 0.01
+        epsilon = self.basedistribution.sample(batch_size)# * 0.01
         mu_t = (t * x1.T + (1 - t) * x0.T).T
         return mu_t + self.sigma * epsilon
 
     def compute_conditional_vector_field(self, x0, x1):
         return x1 - x0
 
-    def sample_forward(self, t, x, *args, **kwargs):
-        while True:
-            t = t.repeat(len(x))
-            return self.forward(t, x)
+    def sample_batch(self, times, batch):
+        n_steps = len(times)
 
-    def sample(self, example_batch):
-        print("Sampling ... ")
-        x0 = self.basedistribution.sample(len(example_batch)).reshape(
-            example_batch.pos.shape
-        )
-
-        node = NeuralODE(
-            SampleHandler(example_batch, self.sample_forward),
-            solver="dopri5",
-            sensitivity="adjoint",
-            atol=1e-4,
-            rtol=1e-4,
-        )
-
+        # simple Euler solver
         with torch.no_grad():
-            traj = node.trajectory(x0, t_span=torch.linspace(0, 1, 100))
-            return traj[-1].reshape(len(example_batch), -1, 3).numpy()
+            traj = torch.zeros((n_steps, *batch.pos.shape), device=batch.pos.device)
+            for i, t in enumerate(times):
+                ts = torch.ones_like(batch.atom_idx)*t
+                velocity = self(ts, batch)
+                batch.pos = batch.pos + velocity*(times[i+1]-times[i]) if i < n_steps-1 else batch.pos
+                traj[i] = batch.pos
 
+        traj = traj.view(n_steps, -1, 15, 3)
+        return traj
+    
 
-class SampleHandler:
-    def __init__(self, batch, forward):
-        self.forward = forward
-        self.batch = batch
+    def sample_batch_dlogp(self, times, batch):
+        # TODO: implement dlogp sampling
+        pass
 
-    def __call__(self, t, pos, *args, **kwargs):
-        self.batch.pos = pos
-        return self.forward(t, self.batch)
+ 
+    def sample(self, loader, n_steps=100, dlogp=False):
+        # TODO: add dlogp to sampling.
+        trajs = []
+        for i, batch in enumerate(loader):
+            times = torch.linspace(0, 1, n_steps)
+            traj = self.sample_batch(times, batch)
+            trajs.append(traj)
+            print(f"Batch {i+1}/{len(loader)} sampled", end="\r")
+
+        trajs = torch.cat(trajs, dim=1)
+        return trajs
+    
